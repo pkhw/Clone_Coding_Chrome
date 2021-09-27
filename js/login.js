193959d6d55e4580690d764600f53a274093811a
pkhw <heewookm0@gmail.com>
2021-09-27 12:12:47 +0900

    click event (alart function)

---
+++ js/login.js
@@ -0,0 +1,16 @@
+const aboutBtn = document.getElementById('about-btn')
+const findEmail = document.querySelector('.forgot-email')
+const policyBtn = document.querySelector('.policy')
+const createAccount = document.querySelector('.create')
+const nextBtn = document.querySelector('.next-btn')
+
+
+function alarm() {
+	alert('this button is not supported in brower')
+}
+
+aboutBtn.addEventListener('click', alarm)
+findEmail.addEventListener('click', alarm)
+policyBtn.addEventListener('click', alarm)
+createAccount.addEventListener('click', alarm)
+nextBtn.addEventListener('click', alarm)
---
+++ js/script.js
@@ -0,0 +1,12 @@
+const bookmarkBtn = document.getElementById('bookmark-btn')
+const recentBtn = document.getElementById('recent-btn')
+const topBtn = document.querySelector('.top-btn')
+
+function alarm() {
+	alert('this button is not supported in browser.')
+}
+
+bookmarkBtn.addEventListener('click', alarm)
+recentBtn.addEventListener('click', alarm)
+
+
