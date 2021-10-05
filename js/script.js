a8370ff150e27cc15d61ebc9358f8d2696252bed
pkhw <heewookm0@gmail.com>
2021-10-05 23:34:54 +0900

    bookmark revise & chrome setting btn add

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const bookmarkBtn = document.getElementById('bookmark-btn')
 const recentBtn = document.getElementById('recent-btn')
 const topBtn = document.querySelector('.top-btn')
+const chromeSettingBtn = document.querySelector('.chrome-setting')
 
 function alarm() {
 	alert('this button is not supported in browser.')
@@ -8,5 +9,6 @@ function alarm() {
 
 bookmarkBtn.addEventListener('click', alarm)
 recentBtn.addEventListener('click', alarm)
+chromeSettingBtn.addEventListener('click', alarm)
 
 
